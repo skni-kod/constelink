b3a80783e24cd9ba6ec06ecfd013bcc516fa59d4
Mateusz Aliyev <82393815+mateuszaliyev@users.noreply.github.com>
2023-10-08 14:58:54 +0200

    fix: add `href` to join project button

--- src/app/(application)/projects/[id]/page.tsx
+++ src/app/(application)/projects/[id]/page.tsx
@@ -62,7 +62,7 @@ const ProjectPage = async ({ params: { id } }: ProjectPageProps) => {
               {project.description}
             </p>
             <div className="my-6 flex flex-col gap-2 sm:flex-row">
-              <Link className={button()} href="/">
+              <Link className={button()} href={`/projects/${id}/chat`}>
                 <Plus className="h-5 w-5" />
                 Join
               </Link>
